f6484ed93e40caee0a3a554c3a0855426f972950
Wildan Nursahidan <edpwildan@gmail.com>
2024-11-12 20:31:15 +0700

    add cli comment

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function Home() {
       </div>
       <span className="flex flex-row items-start sm:gap-2 gap-0.5 text-muted-foreground text-md mt-7 -mb-12 max-[800px]:mb-12 font-code text-base font-medium">
         <TerminalSquareIcon className="w-5 h-5 mr-1 mt-0.5" />
-        {"git clone https://github.com/mywildancloud/docubook.git"}
+        {"npx create-docubook <project-directory>"}
       </span>
     </div>
   );
